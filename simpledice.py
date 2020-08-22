1ab5a14f6546be1849c47d8e3021796d0a2c417a
Bruno Ruy <brunoruy79@gmail.com>
2020-08-22 13:07:55 -0300

    Adicionado funionalidades ao arquivo .py

---
+++ simpledice.py
@@ -0,0 +1,21 @@
+def simpledice():
+    from random import randint
+    n = True
+    r = []
+    while n:
+        print('-='*20)
+        d = int(input('Quantos dados quer jogar: '))
+        l = int(input('Quer dados de quantos lados: '))
+        for i in range(1,d+1):
+           r.append(randint(1,l))
+        print(r)
+        while True:
+            n1 = input('Quer jogar novamente? [S/N] ').strip().upper()[0]
+            if n1 in 'SsnN':
+                break
+        if n1 == 'N':
+            n = False
+        r = []
+    print('-='*20)
+    print('Obrigado por jogar')
+simpledice()
